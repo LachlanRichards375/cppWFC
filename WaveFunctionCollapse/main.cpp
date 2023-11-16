89f84fd013002cce4b9a0cbb21289840988aebd9
LachlanRichards375 <lach1an@tpg.com.au>
2023-11-16 17:33:39 +1030

    MAIN: Updated main to make use of WFCRule_Add_CellIsNot

--- WaveFunctionCollapse/main.cpp
+++ WaveFunctionCollapse/main.cpp
@@ -1,13 +1,40 @@
 #pragma once
 #include "WFCExternRunner.h"
 
+
+void createRules() {
+	//1 Grass
+	//2 Sand
+	//4 Water
+	WFCPosition* IgnoreWest[]{ new WFCPosition(-1,0), new WFCPosition(-1,1), new WFCPosition(-1,-1) };
+	WFCPosition* IgnoreEast[]{ new WFCPosition(1,0), new WFCPosition(1,1), new WFCPosition(1,-1) };
+	WFCPosition* IgnoreAllAround[]{ new WFCPosition(1,0), new WFCPosition(1,1), new WFCPosition(1,-1), new WFCPosition(-1,0), new WFCPosition(-1,1), new WFCPosition(-1,-1), new WFCPosition(0,1), new WFCPosition(0,-1) };
+
+	//Grass cant have Sand West
+	WFCRule_Add_CellIsNot((unsigned long)1, (unsigned long)2, IgnoreWest);
+	//Grass can't have Water anywhere
+	WFCRule_Add_CellIsNot((unsigned long)1, (unsigned long)4, IgnoreAllAround);
+
+	//Sand can't have Water West
+	WFCRule_Add_CellIsNot((unsigned long)2, (unsigned long)4, IgnoreWest);
+	//Sand can't have Grass East
+	WFCRule_Add_CellIsNot((unsigned long)2, (unsigned long)1, IgnoreEast);
+
+	//Water cant have Sand East
+	WFCRule_Add_CellIsNot((unsigned long)4, (unsigned long)2, IgnoreEast);
+	//Water can't have Grass anywhere
+	WFCRule_Add_CellIsNot((unsigned long)4, (unsigned long)1, IgnoreAllAround);
+}
+
 int main(int argc, char* argv[]) {
 	IWFCCollapseMethod* collapse = Threaded2DCollapse_Create();
-	IWFCGrid* grid = Grid2D_Create(5, 5);
+	IWFCGrid* grid = Grid2D_Create(new WFCPosition(5, 5));
 	IWFCManager* manager = IWFCManager_Create(collapse, grid);
 
+	createRules();
+
 	IWFCManager_Run(manager);
-	
+
 	system("pause");
 
 	return 0;
