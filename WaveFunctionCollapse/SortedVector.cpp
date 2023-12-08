a001edf7a58ed5aa69ca22ebcc8a038b30098eb4
LachlanRichards375 <lach1an@tpg.com.au>
2023-12-08 11:11:48 +1030

    CHORE: Cleaned up old cout statements

--- WaveFunctionCollapse/SortedVector.cpp
+++ WaveFunctionCollapse/SortedVector.cpp
@@ -17,7 +17,6 @@ void SortedVector::insert(WFCCell* value)
     //ZoneScopedN("insert");
     int bitsInDomain = value->CalculateEntropy();
     sortedData[bitsInDomain].push_back(value);
-    std::cout << "Setting entropyID of " << value->GetPosition()->to_string() << " to ["<< bitsInDomain << "][" << std::to_string(sortedData[bitsInDomain].size() - 1) << "]" << std::endl;
     value->SetEntropyID(sortedData[bitsInDomain].size()-1);
     ++vectorSize;
 }
@@ -34,7 +33,7 @@ WFCCell* SortedVector::pop() {
     int randomIndex = rand() % (*listWorkingOn).size();
     WFCCell* item{ (*listWorkingOn)[randomIndex] };
     (*listWorkingOn).erase((*listWorkingOn).begin() + randomIndex);
-    ResetEntropyID(listIndex, randomIndex);
+    ResetEntropyID(listIndex, randomIndex-1);
     --vectorSize;
     return item;
 }
@@ -48,7 +47,7 @@ WFCCell* SortedVector::popSpecific(WFCPosition* position, int numBits)
             WFCCell* ptr = (*it);
             sortedData[numBits].erase(it);
             //Reset the entropy id in the current bitMask
-            ResetEntropyID(numBits, index);
+            ResetEntropyID(numBits, index-1);
             --vectorSize;
             return ptr;
         }
@@ -69,31 +68,22 @@ void SortedVector::sort() {
             //remove the old copy
 
         //Update all the old EntropyID within the vector we just updated
+
     for (int NumOfBits = 1; NumOfBits < sortedData.size(); ++NumOfBits) {
-        std::vector<WFCCell*> toReinsert{};
         //If there are dirty cells to clean
         if (dirtyData[NumOfBits].size() == 0) {
             continue;
         }
 
-        std::sort(dirtyData[NumOfBits].begin(), dirtyData[NumOfBits].end());
+        std::vector<WFCCell*> toReinsert{};
 
-        //debug statements
-        std::cout << "\nDirtyData within the " << NumOfBits << " index. Indexes: ";
-        for (auto i : dirtyData[NumOfBits]) {
-            std::cout << i << ",";
-        }
-        std::cout << " need to be moved" << std::endl;
-
-
+        auto sortedIt = sortedData[NumOfBits].begin();
+        int numToRemoveAtStart = dirtyData[NumOfBits].size();
         //remove all the dirty values
-        for (int numberRemovedFromSorted = 0; numberRemovedFromSorted < dirtyData[NumOfBits].size(); ++numberRemovedFromSorted) {
-            int indexToRemove = dirtyData[NumOfBits][numberRemovedFromSorted] - numberRemovedFromSorted;
-            auto sortedIt = sortedData[NumOfBits].begin();
-
-            std::cout << "Size of sortedData[" << NumOfBits << "] is currently " << sortedData[NumOfBits].size() << std::endl;
-            std::cout << "Moving iterator: (" << dirtyData[NumOfBits][numberRemovedFromSorted] << "-" << numberRemovedFromSorted << ") " << indexToRemove << " places, points to: " << sortedData[NumOfBits][indexToRemove]->GetEntropyID() << std::endl;
-            std::advance(sortedIt, indexToRemove);
+        for (int numberRemovedFromSorted = 0; numberRemovedFromSorted < numToRemoveAtStart; ++numberRemovedFromSorted) {
+            //We need to re-validate the iterator
+            sortedIt = sortedData[NumOfBits].begin();
+            std::advance(sortedIt, dirtyData[NumOfBits][numberRemovedFromSorted] - numberRemovedFromSorted);
 
             toReinsert.push_back(*sortedIt);
             sortedData[NumOfBits].erase(sortedIt);
@@ -112,8 +102,6 @@ void SortedVector::sort() {
         //clear the dirty bitmask
         dirtyData[NumOfBits].clear();
     }
-
-    std::cout << "Finished sorting queue. " << std::endl << std::endl;
 }
 
 void SortedVector::ResetEntropyID(int bitNumToReset, int numToStartAt = 0) {
