14b9b78a5433a1afe92c97ed41caa1c2c796aa15
LachlanRichards375 <lach1an@tpg.com.au>
2023-12-15 11:57:27 +1030

    TEST: Grid2D InitializeTests

--- UnitTests/Grid2dTests.cpp
+++ UnitTests/Grid2dTests.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "Grid2D.h"
+#include "WFCPosition.h"
+#include "IWFCManager.h"
+#include "IWFCCollapseMethod.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,15 +12,24 @@ namespace UnitTests
 	TEST_CLASS(Grid2dTests)
 	{
 	public:
+		WFCPosition expectedSize{ 5,5 };
+		Grid2D grid{ expectedSize };
+		IWFCCollapseMethod collapse{ };
+		IWFCManager manager{ &collapse, &grid, 1 };
 		TEST_METHOD(Constructor)
 		{
 			//Do stuff
 			Assert::IsTrue(true);
-			WFCPosition expectedSize{ 5,5 };
-			Grid2D grid{ expectedSize };
+			Grid2D constructorGrid{ expectedSize };
 
-			Assert::AreEqual(grid.GetSize(), expectedSize);
-			Assert::IsNull(grid.GetCell(new WFCPosition(2, 2)));
+			Assert::AreEqual(constructorGrid.GetSize().x, expectedSize.x);
+			Assert::AreEqual(constructorGrid.GetSize().y, expectedSize.y);
+		}
+		TEST_METHOD_INITIALIZE(initialize) {
+			grid.Initialize(&manager);
+		}
+		TEST_METHOD(PostInitialize){
+			Assert::IsNotNull(grid.GetCell(new WFCPosition(2, 2)));
 		}
 	};
 }
